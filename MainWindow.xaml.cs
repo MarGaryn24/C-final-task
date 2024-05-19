1818350f54961ab11f15a6dc2d33bd9cb346462b
MarGaryn24 <cfif22848@gmail.com>
2024-05-20 00:15:51 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Final_Project
+{
+    public partial class MainWindow : Window
+    {
+        private Password_Manager _passwordManager;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _passwordManager = new Password_Manager();
+        }
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            var password = PasswordTextBox.Text;
+            if (!string.IsNullOrEmpty(password))
+            {
+                _passwordManager.EncryptAndSavePassword(password);
+                ResultTextBlock.Text = "Password encrypted and saved.";
+            }
+            else
+            {
+                ResultTextBlock.Text = "Please enter a password.";
+            }
+        }
+        private async void BruteForceButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResultTextBlock.Text = "Brute force attack in progress...";
+            await Task.Run(() =>
+            {
+                var result = _passwordManager.BruteForceAttack();
+                Dispatcher.Invoke(() => ResultTextBlock.Text = result);
+            });
+        }
+    }
+}
---
+++ Password_Manager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class Password_Manager
+{
+    private const string Salt = "DyL9TS5PalSd"; // Static salt
+    private const string FilePath = "Saved_Password.txt";
+
+    public void EncryptAndSavePassword(string password)
+    {
+        var encryptedPassword = EncryptPassword(password);
+        File.WriteAllText(FilePath, encryptedPassword);
+    }
+
+    private string EncryptPassword(string password)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            var saltedPassword = password + Salt;
+            var saltedPasswordBytes = Encoding.UTF8.GetBytes(saltedPassword);
+            var hashBytes = sha256.ComputeHash(saltedPasswordBytes);
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+
+    public string BruteForceAttack()
+    {
+        var encryptedPassword = File.ReadAllText(FilePath);
+        var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+        string foundPassword = null;
+        var possibleComponents = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Parallel.ForEach(GeneratePossiblePasswords(possibleComponents, 6), (password, state) =>
+        {
+            if (EncryptPassword(password) == encryptedPassword)
+            {
+                foundPassword = password;
+                state.Stop();
+                stopwatch.Stop(); // Stop the stopwatch when the password is found
+            }
+        });
+
+        return foundPassword != null ? $"Password found: {foundPassword} in {stopwatch.Elapsed}" : "Password not found";
+    }
+
+    private IEnumerable<string> GeneratePossiblePasswords(string chars, int maxLength)
+    {
+        var charArray = chars.ToCharArray();
+        for (int length = 1; length <= maxLength; length++)
+        {
+            foreach (var combination in GetCombinations(charArray, length))
+            {
+                yield return new string(combination);
+            }
+        }
+    }
+
+    private IEnumerable<char[]> GetCombinations(char[] chars, int length)
+    {
+        if (length == 1)
+        {
+            foreach (var c in chars)
+            {
+                yield return new[] { c };
+            }
+        }
+        else
+        {
+            foreach (var c in chars)
+            {
+                foreach (var combination in GetCombinations(chars, length - 1))
+                {
+                    yield return (new[] { c }).Concat(combination).ToArray();
+                }
+            }
+        }
+    }
+}
